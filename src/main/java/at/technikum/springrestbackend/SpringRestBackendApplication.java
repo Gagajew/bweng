bf8ad6670bda8fd17b1f884d54fae4cd60c44029
PeterWolfframm <servus@peterwolfframm.com>
2025-12-10 11:17:00 +0100

    added enableconfigproperties

--- src/main/java/at/technikum/springrestbackend/SpringRestBackendApplication.java
+++ src/main/java/at/technikum/springrestbackend/SpringRestBackendApplication.java
@@ -1,11 +1,14 @@
 package at.technikum.springrestbackend;
 
+import at.technikum.springrestbackend.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("*")
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class SpringRestBackendApplication {
 
     public static void main(String[] args) {
