55d1f2ca5a171961e56193dc20f6ce5842d68033
AliElShebli <elshebli.ali11@gmail.com>
2026-01-24 15:22:08 +0100

    Add file handling with MinIO + user country/profile placeholder

---
+++ src/main/java/at/technikum/springrestbackend/storage/minio/MinioProperties.java
@@ -0,0 +1,27 @@
+package at.technikum.springrestbackend.storage.minio;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "minio")
+public class MinioProperties {
+    private String url;
+    private int port;
+    private String accessKey;
+    private String secretKey;
+    private String bucket;
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+
+    public int getPort() { return port; }
+    public void setPort(int port) { this.port = port; }
+
+    public String getAccessKey() { return accessKey; }
+    public void setAccessKey(String accessKey) { this.accessKey = accessKey; }
+
+    public String getSecretKey() { return secretKey; }
+    public void setSecretKey(String secretKey) { this.secretKey = secretKey; }
+
+    public String getBucket() { return bucket; }
+    public void setBucket(String bucket) { this.bucket = bucket; }
+}
